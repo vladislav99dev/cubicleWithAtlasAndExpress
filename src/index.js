04f8658544d21cbae61b1b5f300c46121294631c
vladislav99dev <vladislavcho99@abv.bg>
2022-07-23 12:50:04 +0300

    setting up the router

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ const path = require('path');
 
 const initHandlebars = require('./config/initHandlebars')
 const initDatabase = require('./config/initDatabase')
+const router = require('./router')
 const { PORT, dbConnection} = require('./constants')
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "./static")))
 initHandlebars(app)
-
-app.get('/',(req,res) => {
-    res.render('index')
-})
+app.use(router);
 
 initDatabase(dbConnection)
 .then((res) => {
---
+++ src/router.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const homeController = require('./controllers/homeController')
+
+router.use('/', homeController)
+
+
+
+module.exports = router
