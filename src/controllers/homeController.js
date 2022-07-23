4c4c75f5b3d439a9efcff1fc95c39a114f706926
vladislav99dev <vladislavcho99@abv.bg>
2022-07-23 14:32:02 +0300

    displaying cubes from the database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
-router.get('/', (req,res) => {
-    res.render('index')
+const cubeService = require('../services/cubeService');
+
+router.get('/', async(req,res) => {
+    const allCubes = await cubeService.getAll();
+    res.render('index', {allCubes})
 })
 router.get('/about', (req,res) => {
     res.render('about')
