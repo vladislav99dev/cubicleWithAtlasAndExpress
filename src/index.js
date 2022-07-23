3531f45b52843ce5d6bb1b18a47b6541d79bf4a4
vladislav99dev <vladislavcho99@abv.bg>
2022-07-23 13:46:19 +0300

    set up urlencoded

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const path = require('path');
 
+
 const initHandlebars = require('./config/initHandlebars')
 const initDatabase = require('./config/initDatabase')
 const router = require('./router')
-const { PORT, dbConnection} = require('./constants')
+const { PORT, dbConnection } = require('./constants')
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "./static")))
+app.use(express.urlencoded({extended: true}))
+
+
 initHandlebars(app)
 app.use(router);
+
 
 initDatabase(dbConnection)
 .then((res) => {
