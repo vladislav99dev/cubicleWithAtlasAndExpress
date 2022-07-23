f7759dd2101ddc333d6799797f9be7fba0bebc3e
vladislav99dev <vladislavcho99@abv.bg>
2022-07-23 13:06:06 +0300

    added details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 router.get('/create', (req,res) => {
     res.render('create')
 })
-router.get('/details/', (req,res) => {
-    res.render('create')
+router.get('/details/:cubeId', (req,res) => {
+    res.render('details')
 })
 
 module.exports = router
