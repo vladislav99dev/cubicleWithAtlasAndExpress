258cd3b38ada2c085f10b9e2b021cea0bde24372
vladislav99dev <vladislavcho99@abv.bg>
2022-07-24 22:00:51 +0300

    ,modified accessory model

--- src/models/Accessory.js
+++ src/models/Accessory.js
@@ -1,23 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const accessorySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLength: [3, 'Name should have at least 3 characters.']
-    },
-    imageUrl: {
-        type: String,
-        required: true,
-        validate: [/^https:\/\/\S+/, 'The url you provided is not in the expected format.']
-    },
-    descriiption: {
-        type: String,
-        required: true,
-        minLength: [8, 'Description should have at least 8 characters.']
-    },
-})
+  name: {
+    type: String,
+    required: [true, "All inputs should be filled"],
+    minLength: [3, "Name should have at least 3 characters."],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "All inputs should be filled"],
+    validate: [
+      /^https:\/\/\S+/,
+      "The url you provided is not in the expected format.",
+    ],
+  },
+  description: {
+    type: String,
+    required: [true, "All inputs should be filled"],
+    minLength: [8, "Description should have at least 8 characters."],
+  },
+});
 
-const Accessory = mongoose.model('Accesory', accessorySchema)
+const Accessory = mongoose.model("Accesory", accessorySchema);
 
 module.exports = Accessory;
