f428872e939ce1597e40f2e07557ebdca966fef9
vladislav99dev <vladislavcho99@abv.bg>
2022-07-24 19:37:03 +0300

    added some extra validations to the cubeSchema

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -4,14 +4,20 @@ const cubeSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minLength: [3, 'Name should have at least 3 characters.']
   },
   description: {
     type: String,
     required: true,
+    minLength: [8, 'Description should have at least 2 characters.']
+
   },
   imageUrl: {
     type: String,
     required: true,
+    validate: [/^https:\/\/\S+/, 'The url you provided is not in the expected format.']
+    
+
   },
   difficulty: {
     type: String,
