358614336d47cc749972a7efa61e0625a69a8cf5
vladislav99dev <vladislavcho99@abv.bg>
2022-07-24 22:01:25 +0300

    added createAccessory functionality

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const accessoryService = require("../services/accessoryService");
 
 const displayCreatePage = (req, res) => {
   res.render("createAccessory");
@@ -6,7 +7,20 @@ const displayCreatePage = (req, res) => {
 
 const createAccessoryHandler = (req, res) => {
   const { name, description, imageUrl } = req.body;
-  res.end();
+  if (name && description && imageUrl) {
+    console.log(name,description,imageUrl)
+    accessoryService
+      .create(name, description, imageUrl)
+      .then((dbResponse) => {
+        res.redirect("/");
+      })
+      .catch((err) => {
+        res.render("createAccessory", { err: err.message });
+      });
+  } else {
+    let err = "All fileds should be filled.";
+    res.render("createAccessory", { err });
+  }
 };
 
 router.get("/create", displayCreatePage);
