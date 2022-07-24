aa46e9ab6e4a40586a20f87882e201e7f7b6486e
vladislav99dev <vladislavcho99@abv.bg>
2022-07-24 21:44:42 +0300

    adding buttoon for creating accessory

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,23 +1,21 @@
-const Cube = require('../models/Cube')
+const Cube = require("../models/Cube");
 
-
-const create = (name,description,imageUrl, difficulty) => {
-    return Cube.create({name,description,imageUrl,difficulty})
-}
+const create = (name, description, imageUrl, difficulty) => {
+  return Cube.create({ name, description, imageUrl, difficulty });
+};
 
 const getAll = () => {
-    return Cube.find().lean()
-}
+  return Cube.find().lean();
+};
 
 const findById = (id) => {
-    return Cube.findById(id)
-}
-
+  return Cube.findById(id);
+};
 
 const cubeService = {
-    create,
-    getAll,
-    findById
-}
+  create,
+  getAll,
+  findById,
+};
 
-module.exports = cubeService
+module.exports = cubeService;
