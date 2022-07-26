fbd2c0bf6941ad2e546b64bba07f2682a2d13dca
vladislav99dev <vladislavcho99@abv.bg>
2022-07-26 21:43:14 +0300

    Update cubeAccessoryController.js

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -2,37 +2,33 @@ const router = require("express").Router();
 
 const cubeService = require("../services/cubeService");
 const accessoryService = require("../services/accessoryService");
+const cubeAccessoryService = require("../services/cubeAccessoryService");
 
-router.get("/:cubeId", async(req, res) => {
-    try {
-        const cube = await cubeService.findById(req.params.cubeId)
-        const accessories = await accessoryService.getAll();
-        res.render('attachAccessory', {cube, accessories})
-    }
-    catch(err){
-        res.render('404', {err: err.message})
-    }
-});
+const displayAttachAccessoryPage = async (req, res) => {
+  try {
+    const cube = await cubeService.findById(req.params.cubeId);
+    const accessories = await accessoryService.getAll();
+    res.render("attachAccessory", { cube, accessories });
+  } catch (err) {
+    res.render("404", { err: err.message });
+  }
+};
+
+const attachAccessoryHandler = (req, res) => {
+  const accessoryId = req.body.accessory;
+  const cubeId = req.params.cubeId;
+  cubeAccessoryService
+    .addAccessory(cubeId, accessoryId)
+    .then((cube) => {
+      console.log(cube);
+      res.redirect(`/cube/details/${cube._id}`);
+    })
+    .catch((err) => {
+      res.render("404", { err: err.message });
+    });
+};
+
+router.get("/:cubeId", displayAttachAccessoryPage);
+router.post("/:cubeId", attachAccessoryHandler);
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const cubeId = req.params.cubeId;
-// cubeService
-//   .findById(cubeId)
-//   .then((cube) => {
-//     res.render("attachAccessory",  cube );
-//   })
-//   .catch((err) => {});
